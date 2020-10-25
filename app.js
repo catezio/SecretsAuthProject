07ffef22c26942e0f0c82430ef75dd99aebf6d9f
redpanda <assassinak19@gmail.com>
2020-10-25 15:12:10 +0530

    connect atlas cluster for DB

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect("mongodb+srv://admin:admin@cluster0.kla77.mongodb.net/userDB", { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.set('useCreateIndex', true);
 
 const userSchema = new mongoose.Schema({
